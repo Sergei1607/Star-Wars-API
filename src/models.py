b04bb4a9d61b9d07307c8e1ccb336a7104345b02
Sergei1607 <sergei.redondo@gmail.com>
2021-04-11 02:51:03 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
 
 db = SQLAlchemy()
 
@@ -17,3 +18,33 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    rotation_period = db.Column(Integer, nullable=True)
+    orbital_period = db.Column(Integer, nullable=True)
+    diameter = db.Column(Integer, nullable=True)
+    climate = db.Column(String(250), nullable=True)
+    gravity = db.Column(String(250), nullable=True)
+    terrain = db.Column(String(250), nullable=True)
+    surface_water = db.Column(Integer, nullable=True)
+    population = db.Column(Integer, nullable=True)
+
+class Characters(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    height = db.Column(Integer, nullable=True)
+    mass = db.Column(Integer, nullable=True)
+    hair_color = db.Column(String(250), nullable=True)
+    skin_color = db.Column(String(250), nullable=True)
+    eye_color = db.Column(String(250), nullable=True)
+    birth_year = db.Column(Integer, nullable=True)
+    gender = db.Column(String(250), nullable=True)
+
+class Favorites(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    usuario_id = db.Column(Integer, ForeignKey(User.id))
+    planeta_id = db.Column(Integer, ForeignKey(Planets.id))
+    personaje_id = db.Column(Integer, ForeignKey(Characters.id))
